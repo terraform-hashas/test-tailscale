5f97db7fab8a62a9a1529b3b9ed7a61c4e45983e
bhashas <116110608+bhashas@users.noreply.github.com>
2026-03-18 07:54:52 +0100

    Refactor Proxmox VM configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,47 @@
-resource "proxmox_vm_qemu" "vm_test" {
-  name        = "vm-test-tailscale"
-  target_node = "pve-1"
-  vmid        = 505
-
-  clone       = "ubuntu-22.04-template"
-  full_clone  = true
-
-  cores   = 1
-  memory  = 1024
-  cpu     = "host"
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  # Voici la correction pour la version 3.x du provider
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          size    = "10G"
-          storage = "local-lvm"
-        }
-      }
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.66.1" # Version stable actuelle
     }
   }
 }
+
+provider "proxmox" {
+  endpoint  = "https://100.108.39.48:8006/"
+  api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
+  insecure  = true
+}
+
+resource "proxmox_virtual_environment_vm" "vm_test" {
+  name      = "vm-test-tailscale-bpg"
+  node_name = "pve-1"
+  vm_id     = 505
+
+  clone {
+    vm_id = 9000 # Ton template ID
+    full  = true
+  }
+
+  cpu {
+    cores = 1
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 10
+  }
+}
+
+variable "pm_api_token_id" {}
+variable "pm_api_token_secret" {}
