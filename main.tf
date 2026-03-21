c0fd40bd05316037c727f02aee70c4937f48c21e
bhashas <b.hashas@laposte.net>
2026-03-21 19:25:32 +0100

    fix: change VM ID to 506 to force clean recreation

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "proxmox" {
 resource "proxmox_virtual_environment_vm" "vm_test" {
   name      = "vm-test-tailscale-final"
   node_name = "pve-1"
-  vm_id     = 505
+  vm_id     = 506
   started   = true
   on_boot   = false
 
